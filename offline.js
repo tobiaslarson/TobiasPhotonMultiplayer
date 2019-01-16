11081b4b444fa4ed3e9f9d1b363fcc078dc2e050
tobiaslarson <tobias.larson@3cad.se>
2019-01-16 11:16:12 +0100

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,11 +1,14 @@
 ﻿{
-	"version": 1547632646,
+	"version": 1547633695,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"bg.png",
+		"fish-sheet0.png",
+		"fish-sheet1.png",
+		"fish-sheet2.png",
+		"bg-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
